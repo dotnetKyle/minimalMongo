dfe3548557ea1dcf492df2f3811a1f5bf3b8bdc4
Kyle Burrows <dotnetKyle@gmail.com>
2021-08-19 20:53:17 -0400

    Added shortcuts to the filterDefinitionBuilder (and others)

--- src/MongoQ/MongoQRepo.cs
+++ src/MongoQ/MongoQRepo.cs
@@ -10,6 +10,17 @@ namespace MongoQ
         protected IMongoCollection<T> _collection { get; private set; }
         protected MongoCollectionSettings _collectionSettings { get; private set; }
 
+        /// <summary>Filter Definition Builder</summary>
+        protected FilterDefinitionBuilder<T> _filter => Builders<T>.Filter;
+        /// <summary>Update Definition Builder</summary>
+        protected UpdateDefinitionBuilder<T> _update => Builders<T>.Update;
+        /// <summary>Sort Definition Builder</summary>
+        protected SortDefinitionBuilder<T> _sort => Builders<T>.Sort;
+        /// <summary>Projection Definition Builder</summary>
+        protected ProjectionDefinitionBuilder<T> _projection => Builders<T>.Projection;
+        /// <summary>Index Keys Definition Builder</summary>
+        protected IndexKeysDefinitionBuilder<T> _indexKeys => Builders<T>.IndexKeys;
+
         public MongoQRepo(string collectionName, MongoCollectionSettings mongoCollectionSettings = null)
         {
             if (string.IsNullOrWhiteSpace(collectionName))
